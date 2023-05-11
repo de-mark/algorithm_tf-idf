45252fb733b9933073e29134169a46aff49c3b44
de-mark <data.engineer.mark@gmail.com>
2023-05-11 07:37:06 -0700

    Added example text files to read in for tf-idf

---
+++ main.py
@@ -0,0 +1,12 @@
+# Using TextBlob, a Python library for processing text data
+from textBlob import TextBlob
+import math
+
+def main():
+    print("=" * 20)
+    print("TF-IDF EXAMPLE")
+    print("=" * 20)
+    print("This program will automatically pull all .txt files from your documents_to_read folder and will report the top words in argument as well as their TF-IDF scores")
+    
+
+main()
